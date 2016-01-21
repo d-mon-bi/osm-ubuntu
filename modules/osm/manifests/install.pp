048ab0c47aeb1ba2a192059465b1bc0f8907de0c
d-mon-bi <daniel.montero+gh@gmail.com>
2016-01-21 09:07:13 -0600

    not working with master, can't serve files

--- modules/osm/manifests/install.pp
+++ modules/osm/manifests/install.pp
@@ -1,8 +1,27 @@
 class osm::install{
   $as_vagrant = 'sudo -u vagrant -H bash -l -c'
+  $install_sh = "#!/bin/bash
+git clone --depth=1 https://github.com/openstreetmap/openstreetmap-website.git
+cd openstreetmap-website
+bundle install
+cp config/example.application.yml config/application.yml
+cp config/example.database.yml config/database.yml
+sudo -u postgres -i
+createuser -s <username>
+exit
+bundle exec rake db:create
+psql -d openstreetmap -c "CREATE EXTENSION btree_gist"
+cd db/functions
+make libpgosm.so
+cd ../..
+psql -d openstreetmap -c "CREATE FUNCTION maptile_for_point(int8, int8, int4) RETURNS int4 AS '`pwd`/db/functions/libpgosm', 'maptile_for_point' LANGUAGE C STRICT"
+psql -d openstreetmap -c "CREATE FUNCTION tile_for_point(int4, int4) RETURNS int8 AS '`pwd`/db/functions/libpgosm', 'tile_for_point' LANGUAGE C STRICT"
+psql -d openstreetmap -c "CREATE FUNCTION xid_to_int4(xid) RETURNS int4 AS '`pwd`/db/functions/libpgosm', 'xid_to_int4' LANGUAGE C STRICT"
+bundle exec rake db:migrate
+"
 
   file { '/home/vagrant/install.sh':
-	source => "puppet:///modules/osm/files/install.sh",
+	content => "$install_sh",
 	mode => '700',
   }
   
