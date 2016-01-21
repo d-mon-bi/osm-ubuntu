e26738828862b18e78313c90c6cb854bc8af0992
d-mon-bi <daniel.montero+gh@gmail.com>
2016-01-21 08:56:41 -0600

    corrected install script reference

--- modules/osm/manifests/install.pp
+++ modules/osm/manifests/install.pp
@@ -1,13 +1,14 @@
 class osm::install{
   $as_vagrant = 'sudo -u vagrant -H bash -l -c'
 
-  file { '~/install.sh':
+  file { '/home/vagrant/install.sh':
 	source => "puppet:///modules/osm/files/install.sh",
 	mode => 700,
   }
   
   exec { 'install-osm':
 	path        => '/usr/bin:/usr/sbin:/bin',
-    command	=> "${as_vagrant} '~/install.sh'",
+    command		=> "${as_vagrant} '/home/vagrant/install.sh'",
+	require 	=> File['/home/vagrant/install.sh'],
   }
 }
